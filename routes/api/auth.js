228e91c66f5fa072809b5ddff6813e88c3268fda
ab <admin@admins-MacBook-Air.local>
2022-03-27 21:03:18 -0400

    first commit

---
+++ routes/api/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const auth  = require('../../middleware/auth');
+const User = require('../../models/User');
+
+router.get('/',auth, async(req,res) => {
+    try{
+        const user = await User.findById(req.user.id).select('-password'); 
+        res.json(user);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+});
+
+module.exports = router;
