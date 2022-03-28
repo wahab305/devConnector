a5883a9bafefff0103528e12568db14cd035d7cc
wahab305 <admin@admins-MacBook-Air.local>
2022-03-27 23:57:37 -0400

    adding profile schema

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth'); 
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get('/', (req,res) => res.send('Profile route'));
+
+
+router.get('/me', auth, async(req,res) => {
+    try{
+    const profile = await Profile.findOne({ user: req.user.id}).populate('user',['name','avatar']);
+
+    if(!profile){
+        return res.status(400).json({ msg:'There is no profile'});
+    }
+
+    res.json(profle);
+}catch(err){
+        console.error(err.message);
+        res.status(500)
+        .send('server error');
+
+    }
+});
 
 module.exports = router;
